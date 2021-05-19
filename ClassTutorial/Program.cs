f3ecefdc2aaae23f2a073c31c70f47d516432ff4
Mattea Swain <mmswain@tql.com>
2021-05-19 16:30:33 -0400

    Add project files.

---
+++ ClassTutorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tqlmath = new TqlMath();
+            tqlmath.A = 21;
+            tqlmath.B = 30;
+            var minus = tqlmath.Minus();
+            var sum = tqlmath.Sum();
+
+            Console.WriteLine($"The Sum of {tqlmath.A} and {tqlmath.B} is {sum} ");
+
+            
+            Console.WriteLine($"The difference of {tqlmath.A} and {tqlmath.B} is {minus} ");
+        }
+    }
+}
---
+++ ClassTutorial/TqlMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassTutorial
+{
+    class TqlMath
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int Sum()
+        {
+            return A + B;
+        }
+        public int Minus()
+        {
+            return A - B;
+        }
+           
+    }
+}
